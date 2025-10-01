ee8d87bbad23dd50c2233f30bc7ea40d21a5653b
yunrry <yunryy329@gmail.com>
2025-10-01 13:18:33 +0900

    init

--- _posts/2025-10-01-first-post.md
+++ _posts/2025-10-01-first-post.md
@@ -1,8 +1,9 @@
 ---
-layout: post
+layout: single
 title: "첫 번째 게시글"
 date: 2025-10-01
-tags: [일상, 시작]
+categories: [블로그]
+tags: [시작]
 ---
 
 안녕하세요! 첫 번째 게시글입니다.
@@ -12,7 +13,6 @@ tags: [일상, 시작]
 Markdown으로 쉽게 글을 작성할 수 있습니다.
 
 - 리스트도 가능
-- 이미지 삽입: `![설명](이미지주소)`
-- 코드 블록도 지원
+- 이미지 삽입도 가능
 ```python
 print("Hello, World!")
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "첫 번째 게시글",
-        "excerpt":"안녕하세요! 첫 번째 게시글입니다.   소제목   Markdown으로 쉽게 글을 작성할 수 있습니다.      리스트도 가능   이미지 삽입: ![설명](이미지주소)   코드 블록도 지원 ```python print(“Hello, World!”)  ","categories": [],
-        "tags": ["일상","시작"],
-        "url": "/first-post/",
+        "excerpt":"안녕하세요! 첫 번째 게시글입니다.   소제목   Markdown으로 쉽게 글을 작성할 수 있습니다.      리스트도 가능   이미지 삽입도 가능 ```python print(“Hello, World!”)  ","categories": ["블로그"],
+        "tags": ["시작"],
+        "url": "/%EB%B8%94%EB%A1%9C%EA%B7%B8/first-post/",
         "teaser": null
       }]
