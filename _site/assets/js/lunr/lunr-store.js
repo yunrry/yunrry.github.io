cf0c6e4d0e2821ddc38c2ed7228ffb07925e74f6
yunrry <yunryy329@gmail.com>
2025-10-03 01:55:01 +0900

    remove

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["시작"],
         "url": "/dev/first-post/",
         "teaser": null
+      },{
+        "title": "배열 회전",
+        "excerpt":"배열 왼쪽 회전 시각화 예시: [1,2,3,4,5]를 왼쪽으로 2칸 회전 초기 상태 [1, 2, 3, 4, 5] Step 1: reverse(arr, 0, 1) - 처음 k개 뒤집기 [1, 2, 3, 4, 5] ↓ ↓ [2, 1, 3, 4, 5] Step 2: reverse(arr, 2, 4) - 나머지 뒤집기 [2, 1, 3, 4, 5]...","categories": ["Algorithm"],
+        "tags": ["알고리즘","자료구조","배열"],
+        "url": "/algorithm/array-rotate/",
+        "teaser": null
       }]
